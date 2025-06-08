6eb1187986959bfd90ee2e975fa58e53d654bfbc
Manul <juliakatze@bk.ru>
2025-06-08 16:01:48 +0300

    Задание №6: Изменила метод по логину\разлогину на TestBase. И дополнила TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/steps/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/steps/TestBase.java
@@ -1,24 +1,37 @@
-package ru.stqa.addressbook.steps;
+package ru.stqa.addressbook.tests;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class LogInAndOut {
-    private WebDriver driver;
+public class TestBase {
+    protected WebDriver driver;
 
-    public LogInAndOut(WebDriver driver) {
-        this.driver = driver;
+    @BeforeEach
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1721, 1033));
+        auth();
     }
+
+    @AfterEach
+    public void tearDown() {
+        logout();
+        driver.quit();
+    }
+
     public void auth() {
         driver.get("http://localhost/addressbook/");
-        driver.manage().window().setSize(new Dimension(1721, 1033));
         driver.findElement(By.name("user")).click();
         driver.findElement(By.name("user")).sendKeys("admin");
         driver.findElement(By.name("pass")).sendKeys("secret");
         driver.findElement(By.xpath("//input[@value='Login']")).click();
     }
-    public void logout(){
+
+    public void logout() {
         driver.findElement(By.linkText("Logout")).click();
     }
 }
