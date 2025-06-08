af66d6de090a2a617024aeba99c45b3f95f9c814
Manul <juliakatze@bk.ru>
2025-06-08 15:37:28 +0300

    Задание №5: Сделать тесты для создания и удаления групп в адресной книге

--- sandbox/src/main/java/org/example/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-                System.out.printf("~~~~~~~\nHello, Software-Testing!\nHave a nice day! \n~~~~~~~");
-            }
-        }
