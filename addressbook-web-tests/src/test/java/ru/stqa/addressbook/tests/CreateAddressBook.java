b53c65f960e3cc83e73f5c033cc53770a0aba62d
Manul <juliakatze@bk.ru>
2025-06-08 22:37:31 +0300

    Задание №7: Метод создания Адресной записи(черновик) + assert

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/CreateAddressBook.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/CreateAddressBook.java
@@ -1,12 +1,7 @@
 package ru.stqa.addressbook.tests;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import ru.stqa.addressbook.manager.AddressHelper;
-
-import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CreateAddressBook extends TestBase {
