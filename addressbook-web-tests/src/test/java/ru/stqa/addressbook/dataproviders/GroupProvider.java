bcb534a5333a11033578f63b2b30b1c8bb228201
Manul <juliakatze@bk.ru>
2025-06-22 22:14:08 +0300

    Лекции 4. Group Creation Параметризация. Вынесла в отдельный пакет и класс

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/dataproviders/GroupProvider.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/dataproviders/GroupProvider.java
@@ -1,4 +1,21 @@
 package ru.stqa.addressbook.dataproviders;
 
+import ru.stqa.addressbook.models.GroupData;
+import static ru.stqa.addressbook.tests.TestBase.randomString;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupProvider {
+    public static List<GroupData> groupProvider(){
+        var result = new ArrayList<GroupData>(List.of(
+                new GroupData("group name", "", ""),
+                new GroupData("group name'", "", ""))
+        );
+        for (int i = 0; i<5; i++){
+            result.add(new GroupData(randomString(i*10), randomString(i*10), randomString(i*10)));
+        }
+        return result;
+    }
+
 }
