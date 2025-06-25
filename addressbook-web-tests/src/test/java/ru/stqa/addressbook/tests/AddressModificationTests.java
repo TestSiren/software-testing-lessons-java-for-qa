d0ed2c69d0ec29d45e5bdb0c7db9ea7f705bb731
Manul <juliakatze@bk.ru>
2025-06-25 14:37:21 +0300

    Задание 11. Модификация контактов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/AddressModificationTests.java
@@ -0,0 +1,55 @@
+package ru.stqa.addressbook.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.stqa.addressbook.manager.GroupHelper;
+import ru.stqa.addressbook.models.GroupData;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static ru.stqa.addressbook.comporators.GroupComporators.compareById;
+
+public class AddressModificationTests {
+    @Test
+    void canModifyAddress() {
+        GroupHelper groups = app.groups();
+        if (groups.getGroupsCount() == 0) {
+            groups.createGroup(new GroupData("", "group name", "group header", "group footer"));
+        }
+
+        var oldGroups = groups.getListGroups();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldGroups.size());
+        var testData = new GroupData().withName("modified name");
+
+        groups.modifyGroup(oldGroups.get(index), testData);
+
+        var newGroups = groups.getListGroups();
+        var expectedList = new ArrayList<>(oldGroups);
+        expectedList.set(index, testData.withId(oldGroups.get(index).id()));
+
+        newGroups.sort(compareById);
+        expectedList.sort(compareById);
+
+        Assertions.assertEquals(newGroups, expectedList);
+    }
+    @Test
+    void cannotModifyAddress() {
+        GroupHelper groups = app.groups();
+        if (groups.getGroupsCount() == 0) {
+            groups.createGroup(new GroupData("", "group name", "group header", "group footer"));
+        }
+
+        var oldGroups = groups.getListGroups();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldGroups.size());
+        var testData = new GroupData().withName("modified' name");
+
+        groups.modifyGroup(oldGroups.get(index), testData);
+
+        var newGroups = groups.getListGroups();
+
+        Assertions.assertEquals(newGroups, oldGroups);
+    }
+}
