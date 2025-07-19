1eba8b8d5e7327948cdd0782081490318b300cc6
Manul <juliakatze@bk.ru>
2025-07-19 14:02:00 +0300

    Task15.разбиение на 2 теста тест на создание?

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/AddressAddInGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/AddressAddInGroup.java
@@ -63,20 +63,24 @@ public class AddressAddInGroup extends TestBase {
         GroupHelper groups = app.groups();
         HibernateHelper hbm = app.hbm();
 
-        if (hbm.getGroupsCount() == 0) {
+        //есть 2 группы?  нет -> создать
+        if (hbm.getGroupsCount() <= 1) {
             hbm.createGroup(new GroupData().withName("some name"));
         }
-        if (hbm.getCountContactsWithGroup()==0){
-            addresses.createAddress(new AddressData(), null);
-        }
+        //есть контакты? нет -> создать, да -> Проверить в группе ли они. Если не в группе, то добавить.
 
+
+        //получаем "старый список"
         var oldAddress = hbm.getContactsWithoutGroup();
 
+        //получаем список групп
         var list_groups = hbm.getGroupList();
 
+        //нужно проверить что контакт не в группе А. Если в группе А, взять Б, если в Б, взять А
         var rnd = new Random();
         var rndGroup = rnd.nextInt(list_groups.size());
         var group = list_groups.get(rndGroup);
+
 
         var rndContact = rnd.nextInt(oldAddress.size());
         var address = oldAddress.get(rndContact);
@@ -84,18 +88,20 @@ public class AddressAddInGroup extends TestBase {
         var oldRelated = hbm.getContactsInGroup(group);
         oldRelated.sort(byId);
 
+        // меняем группу контакту
         var filter = "[none]";
         addresses.addressAddToGroup(address, group, filter);
 
         var newRelated = hbm.getContactsInGroup(group);
         newRelated.sort(byId);
 
+        // формируем ожидаемый результат
         var expectedList = new ArrayList<>(oldRelated);
         var modifiedContact = address.withGroup(group.name());
         expectedList.add(modifiedContact);
 
         expectedList.sort(byId);
-
+// постусловие
         Assertions.assertEquals(expectedList, newRelated);
         System.out.println("expectedList: " + expectedList + "\nnewRelated: " + newRelated);
     }
